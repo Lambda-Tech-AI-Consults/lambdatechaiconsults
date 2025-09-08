5a5219448a7c1d64eceb3a5c0ab6c0ebabcf0407
OfficialDustman <officialdustman@gmail.com>
2025-09-08 11:17:28 +0100

    updated services images path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: { unoptimized: true }
+  // images: { unoptimized: true }
 };
 
 export default nextConfig;
